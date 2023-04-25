06c1ebb17775afcf8c03c6a262a5834ba2e012cf
toms-source <gerwincristobal11@gmail.com>
2023-04-25 23:08:17 +0800

    tailwind install

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,46 @@
+<div>
+   <!-- <div class="home text-xl">this is home</div> -->
+
+   <nav>
+      <ul class="h-14 font-sans text-xl font-bold text-sky-500 flex justify-center items-center gap-20">
+         <li class="cursor-pointer">PRODUCTS</li>
+         <li class="cursor-pointer">SERVICES</li>
+         <li class="cursor-pointer">ABOUT</li>
+         <li class="cursor-pointer">CONTACT</li>
+      </ul>
+   </nav>
+
+   <section id="login">
+      <div></div>
+      <div>
+         <div>SIGN IN</div>
+         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+               <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+               Username
+               </label>
+               <input class="shadow appearance-none rounded-lg border  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+            </div>
+
+            <div class="mb-6">
+               <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+               Password
+               </label>
+               <input class="shadow appearance-none rounded-lg border  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
+            </div>
+            <div class="flex items-center justify-between">
+               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+               Google Sign In
+               </button>
+               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+               Sign In
+               </button>
+            
+            </div> 
+            <a class="inline-block align-baseline font-bold text-sm text-black-500 hover:text-black-800" href="#">
+            REGISTER HERE
+            </a>
+  </form>
+      </div>
+   </section>
+</div>
