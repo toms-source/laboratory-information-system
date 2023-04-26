16139a003b8f8e2fdd745464261cabb770b03aec
Raine <lorainemae.rito22@gmail.com>
2023-04-26 11:46:16 +0800

    hi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
@@ -12,19 +11,3 @@ module.exports = {
   plugins: [],
 }
 
-=======
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                landing: "url('/resources/img/undraw_scientist_ft0o.svg')",
-            },
-        },
-    },
-    plugins: [],
-};
->>>>>>> 8ef7b385a3da2548f77a312f56632a1efd1bb07c
