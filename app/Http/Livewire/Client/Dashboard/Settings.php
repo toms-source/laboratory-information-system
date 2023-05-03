2c9fe0b1f565580112f2087e6281d86b52f66089
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-05-03 18:01:13 +0800

    new: contact form, contact model and contact table

--- app/Http/Livewire/Client/Dashboard/Settings.php
+++ app/Http/Livewire/Client/Dashboard/Settings.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Livewire\Client\Dashboard;
 
-use Livewire\Component;
 use App\Models\User;
+use Livewire\Component;
+use App\Models\EmergencyContacts;
 
 class Settings extends Component
 {
+    //TODO: refactor update or create
+    //create delete emergency contact
+    //add validation for emergency contact
+    //add validation for birthday
+    //debug birthday display. it is not displaying the correct date
+    
+    public $user;
+
     public $name;
     public $contact;
     public $address;
@@ -14,47 +23,120 @@ class Settings extends Component
     public $gender;
     public $birthday;
 
+    public $em_contacts = [];
+
     protected $rules = [
         'name' => 'required|min:6',
         'address' => 'required|min:6',
         'contact' => 'required|min:9',
-        'age' => 'min:3',
+        'age' => 'integer',
     ];
 
     public function mount()
-    {
-        $this->name = auth()->user()->name;
-        $this->contact = auth()->user()->contact;
-        $this->address = auth()->user()->address;
-        $this->age = auth()->user()->age;
-        $this->gender = auth()->user()->gender;
-        $this->birthday = auth()->user()->birthday;
-       // dd($this->name);
+    {   
+        $this->user = User::find(auth()->user()->id);
+
+        $this->name = $this->user->name;
+        $this->contact = $this->user->contact;
+        $this->address =$this->user->address;
+        $this->age = $this->user->age;
+        $this->gender = $this->user->gender;
+        $this->birthday = date('d-m-Y', strtotime($this->user->birthday));
+        // dd($this->name);
+
+        foreach ($this->user->emergencyContacts as $contact) {
+            $this->em_contacts[] = $contact;
+        }
+        //dd($this->em_contacts[] = auth()->user()->emergencyContacts);
     }
 
     public function updateSettings()
-    {
+    {   
         $this->validate();
 
-        $this->client_id = auth()->user()->id;
-        $i = 0;
-        $i = $this->client_id;
-        User::find($i)->update([
-        'name' => $this->name,
-        'address'=> $this->address,
-        'contact'=> $this->contact,
-        'age'=> $this->age,
-        'gender'=> $this->gender,
-        'birthday'=> $this->birthday,
+        $this->user->update([
+            'name' => $this->name,
+            'address'=> $this->address,
+            'contact'=> $this->contact,
+            'age'=> $this->age,
+            'gender'=> $this->gender,
+            'birthday'=> $this->birthday,
         ]);
+
+        //https://laravel.com/docs/10.x/eloquent#upserts
+        //updateOrCreate()
+
+        // f{{ orea }}ch($this->em_contacts as $value)
+        // {{{  }}
+        //     if($value->id)
+        //     {
+        //         $value->update([
+        //             'name' => $value->name,
+        //             'relationship' => $value->relationship,
+        //             'address' => $value->address,
+        //             'contact' => $value->contact,
+        //         ]);
+        //     }
+        //     else
+        //     {
+        //         $user->emergencyContacts()->create([
+        //             'name' => $value['name'],
+        //             'relationship' => $value['relationship'],
+        //             'address' => $value['address'],
+        //             'contact' => $value['contact'],
+        //         ]);
+        //     }
+        // }
+        
+
+        //for reference
+        // / Get the difference between $stocks and $stocks_copy
+        // $differences = array_udiff($this->stocks_copy, $this->stocks, function ($a, $b) {
+        //     return strcmp(serialize($a), serialize($b));
+        // });
+
+        // // Delete deleted forms from stocks first. 
+        // if($differences)
+        // {
+        //     foreach($differences as $difference)
+        //     {
+        //         if(array_key_exists('id', $difference))
+        //         {
+        //             // Delete deleted form from stock array
+        //             $this->product->stocks()->find($difference['id'])->delete();
+        //         } 
+        //     }
+        // }
+
+        // foreach($this->stocks as $stock)
+        // {   
+            
+        //     $this->product->stocks()->updateOrCreate(
+        //         [
+        //             'name' => $stock['name'],
+        //             'quantity' => $stock['quantity'],
+        //             'original_price' => $stock['original_price'],
+        //             'selling_price' => $stock['selling_price'],
+        //             'status' => $stock['status'],
+        //         ]
+        //     );
+        // }
 
         session()->flash('success', 'Update successfully!');
     }
+
+    public function addContact()
+    {
+        //or  $this->em_contacts[] = auth()->user()->emergencyContacts;
+        $this->em_contacts[] = new EmergencyContacts();
+    }
+
     public function updated($property)
     {
         $this->validateOnly($property);
     }
 
+    
     public function hide()
     {
         session()->forget('success');
--- resources/views/livewire/client/dashboard/settings.blade.php
+++ resources/views/livewire/client/dashboard/settings.blade.php
@@ -6,7 +6,7 @@
             </div>
         </div>
     @endif
-    <form >
+    <form wire:submit.prevent="updateSettings">
         <h1 class="text-2xl mb-5">Personal Information</h1>
         <div class="grid gap-6 mb-6 md:grid-cols-2">
             <div class="mb-2">
@@ -58,33 +58,41 @@
         <br>
         <hr>
         <br>
-        <h1 class="text-2xl mb-5">Emergency Contact</h1>
-        <div class="grid gap-6 mb-6 md:grid-cols-2">
 
-            <div class="mb-2">
-                <label for="base-input"
-                    class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Name</label>
-                <input type="text" id="base-input"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            </div>
-            <div class="mb-2">
-                <label for="base-input"
-                    class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Relationship</label>
-                <input type="text" id="base-input"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            </div>
-            <div class="mb-2">
-                <label for="base-input"
-                    class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Address</label>
-                <input type="text" id="base-input"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            </div>
-            <div class="mb-2">
-                <label for="base-input"
-                    class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Contact</label>
-                <input type="number" id="base-input"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            </div>
+        <div class="flex justify-between">
+            <h1 class="text-2xl mb-5">Emergency Contact</h1>
+            <button wire:click.prevent="addContact">
+                Add Contact
+            </button>
+        </div>
+        
+        <div class="grid gap-6 mb-6 md:grid-cols-2">
+            @foreach(array_reverse($em_contacts, true) as $index => $em_contact)
+                <div class="mb-2">
+                    <label for="base-input"
+                        class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Name</label>
+                    <input type="text" id="base-input"  wire:model.debounce.500ms="em_contacts.{{ $index }}.name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+                <div class="mb-2">
+                    <label for="base-input"
+                        class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Relationship</label>
+                    <input type="text" id="base-input" wire:model.debounce.500ms="em_contacts.{{ $index }}.relationship"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+                <div class="mb-2">
+                    <label for="base-input"
+                        class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Address</label>
+                    <input type="text" id="base-input" wire:model.debounce.500ms="em_contacts.{{ $index }}.address"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+                <div class="mb-2">
+                    <label for="base-input"
+                        class="block mb-2  text-md font-medium text-gray-900 dark:text-white">Contact</label>
+                    <input type="number" id="base-input" wire:model.debounce.500ms="em_contacts.{{ $index }}.contact"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+            @endforeach
         </div>
         <br>
         <hr>
@@ -117,7 +125,7 @@
             </div>
         </div>
         <div>
-            <button type="submit" wire:click.prevent="updateSettings" class="bg-sky-400 hover:bg-sky-500 py-2 px-4 rounded-md">Save Changes</button>
+            <button type="submit" class="bg-sky-400 hover:bg-sky-500 py-2 px-4 rounded-md">Save Changes</button>
         </div>
     </form>
 </div>
